186745b8e30b23595c9f5d894463a34db8223cf1
Renato Oliveira <renato.aparecido@g.globo>
2021-02-22 15:16:50 -0300

    remove unnecessary debounce

--- app/src/main/java/com/starwars/challenge/features/search/domain/SearchUseCase.kt
+++ app/src/main/java/com/starwars/challenge/features/search/domain/SearchUseCase.kt
@@ -5,5 +5,5 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.debounce
 
 class SearchUseCase(private val repository: DataRepository): ISearchUseCase {
-    override fun execute(query: String): Flow<String> = repository.fetchData(query).debounce(1500L)
+    override fun execute(query: String): Flow<String> = repository.fetchData(query)
 }
